ba5a0dad67fdf7bdf9f250bfced6cb51c8c5490b
galanovaks <galanova14k@yandex.ru>
2022-08-03 16:40:16 +0300

    Added graph structure.

--- graph.h
+++ graph.h
@@ -1,1 +1,47 @@
+#include <stdlib.h>
 
+struct list_vertex
+{
+    int num;
+    int col;
+    struct vertex *next;
+};
+
+struct graph
+{
+    struct list_vertex *v;
+    struct graph *next;
+};
+
+struct graph *create_graph(int ver)
+{
+    int i;
+    struct graph *tmp=malloc(sizeof(*tmp));
+    (*tmp).v=malloc(ver*sizeof((*(*tmp).v)));
+    (*(*tmp).v).num=ver-1;
+    (*(*tmp).v).col=-1;
+    (*(*tmp).v).next=NULL;
+    (*tmp).next=NULL;
+    for (i=ver-2;i>=0;i--)
+    {
+        struct graph *aux=malloc(ver*sizeof(*aux));
+        (*aux).v=malloc(ver*sizeof((*(*aux).v)));
+        (*(*aux).v).num=i;
+        (*(*aux).v).col=-1;
+        (*(*aux).v).next=NULL;
+        (*aux).next=tmp;
+        tmp=aux;
+    }
+    return(tmp);
+}
+
+void output_graph(struct graph *g)
+{
+    struct graph *aux=g;
+    while (g!=NULL)
+    {
+        printf("%d %d\n",(*(*g).v).num,(*(*g).v).col);
+        g=(*g).next;
+    }
+    g=aux;
+}
--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "graph.h"
 #include "solver.h"
+
 int main()
 {
-return 0;
+    int v;
+    scanf("%d", &v);
+    struct graph *g=create_graph(v);
+    output_graph(g);
+    output_graph(g);
+    return 0;
 }
